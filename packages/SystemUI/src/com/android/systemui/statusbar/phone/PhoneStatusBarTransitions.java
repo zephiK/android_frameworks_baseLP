f731a4af91a4acfc0b31e7b068cecab113308247
Jon Haus <haus.xda@gmail.com>
2014-01-21 15:50:05 -0500

    [1/2] Base: Upgrade network traffic meter
    *Updated class name to be less ambiguous
    *Replaced the multi-check in settings with a proper listview
    *Added the ability to view up-stream, down-stream or both
    *Added the option to change how often it refreshes
    *Added a check before it updates the status bar to prevent it from
    refreshing everything needlessly
    *Added icons to show data direction
    *Remove unused style attributes
    
    Icon artwork is courtesy android design downloads
    Some features were inspired by previous work done by 0xD34D
    
    OmniGears: https://gerrit.omnirom.org/#/c/5130/
    
    Change-Id: I9f86292a6adbc5e30967df76fae510be248fc38b
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/res/values/mahdi_dimens.xml
    	packages/SystemUI/res/values/styles.xml
    	packages/SystemUI/src/com/android/systemui/quicksettings/NetworkSpeedTile.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/res/values/simpleaosp_dimens.xml
    	packages/SystemUI/res/values/styles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/res/values/styles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock, mNetworkTraffic;
 
     private Animator mCurrentAnimation;
 
@@ -49,6 +49,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         mBattery = mView.findViewById(R.id.battery);
         mClock = mView.findViewById(R.id.clock);
+        mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
         mCenterClock = mView.findViewById(R.id.center_clock);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
@@ -93,6 +94,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mSignalCluster, newAlpha),
+                    animateTransitionTo(mNetworkTraffic, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mCenterClock, newAlphaBC)
@@ -106,6 +108,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mSignalCluster.setAlpha(newAlpha);
+            mNetworkTraffic.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);
