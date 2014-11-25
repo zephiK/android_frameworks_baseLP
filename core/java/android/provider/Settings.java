9e68d4e36a74079bc1af4601c03c8a0a5743b969
cmorlok <ChristianMorlok@gmail.com>
2014-11-25 13:31:56 +0000

    [1/2] Base: Hide power menu on lockscreen
    Adds an option to enable/disable power menu on lockscreen.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2884,6 +2884,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+         * This preference enables showing the power menu on LockScreen.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
