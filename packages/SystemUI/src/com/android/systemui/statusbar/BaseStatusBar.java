045cfa7497701f45250fb295bc06fbe28b4ff37f
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-12-26 14:29:56 +0000

    HeadsUp: enable by default
    also remove unnecessary code
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1207,8 +1207,6 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     public abstract void resetHeadsUpDecayTimer();
 
-    public abstract void hideHeadsUp();
-
     public abstract void scheduleHeadsUpOpen();
 
     public abstract void scheduleHeadsUpClose();
