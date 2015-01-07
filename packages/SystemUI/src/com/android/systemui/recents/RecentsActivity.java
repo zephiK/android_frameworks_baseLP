d5138ebd29738c06a16ce0343755289ea0e70fd9
zephiK <zephik@gmail.com>
2015-01-07 14:29:59 -0500

    Fix unused space part 2

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -272,7 +272,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         Resources reso = getResources();
 
         if (Settings.System.getInt(getContentResolver(),
-                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) != 1) {
+                    Settings.System.RECENTS_SHOW_HIDE_SEARCH_BAR, 1) != 0) {
         RecentsConfiguration.searchBarSpaceHeightPx = 0;
 
 		}
