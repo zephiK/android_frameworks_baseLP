7a95416f8d1ec5e0b6b561ce4d666a5f51f14248
Scott Mertz <scott@cyngn.com>
2015-01-09 12:06:05 -0800

    MountService: support UMS on any volume

--- services/core/java/com/android/server/MountService.java
+++ services/core/java/com/android/server/MountService.java
@@ -1470,6 +1470,16 @@ class MountService extends IMountService.Stub
         return null;
     }
 
+    private boolean hasUmsVolume() {
+        final StorageVolume[] storageVolumes = getVolumeList();
+        for(StorageVolume volume : storageVolumes) {
+            if (volume.allowMassStorage()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * Constructs a new MountService instance
      *
@@ -1497,9 +1507,7 @@ class MountService extends IMountService.Stub
         userFilter.addAction(Intent.ACTION_USER_REMOVED);
         mContext.registerReceiver(mUserReceiver, userFilter, null, mHandler);
 
-        // Watch for USB changes on primary volume
-        final StorageVolume primary = getPrimaryPhysicalVolume();
-        if (primary != null && primary.allowMassStorage()) {
+        if (hasUmsVolume() || SystemProperties.getBoolean("persist.sys.ums", true)) {
             mContext.registerReceiver(
                     mUsbReceiver, new IntentFilter(UsbManager.ACTION_USB_STATE), null, mHandler);
         }
