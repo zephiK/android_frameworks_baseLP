ddc537588faa565e30657e1782044e42e82403b1
maxwen <max.weninger@gmail.com>
2015-01-16 21:08:00 +0100

    [1/2] base: allow to swap volume buttons rotation based
    Forward port from 4.4 but became more complicated now
    Also check the volume change sound disablement earlier
    
    Change-Id: Ia0795399aefdf7a80c03a9ab5eeb410afca8bc18
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2997,6 +2997,12 @@ public final class Settings {
         * @hide
         */
         public static final String VOLUME_KEY_ADJUST_SOUND = "volume_key_adjust_sound";
+        
+        /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
 	/*
          * Whether to hide the clock, show it in the right or left
