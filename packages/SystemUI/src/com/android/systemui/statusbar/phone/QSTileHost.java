cc8bb643dcfb3f01abce54b01b79d388f4cb4f10
Danesh M <daneshm90@gmail.com>
2015-01-23 18:59:21 -0800

    SystemUI : Add screen timeout tile
    Change-Id: I0854e940f9604dee2fff5093e688ef3a17b258a9
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -284,6 +285,15 @@ public class QSTileHost implements QSTile.Host {
                 return new HotspotTile(this);
             case QSConstants.TILE_NOTIFICATIONS:
                 return new NotificationsTile(this);
+<<<<<<< HEAD
+=======
+            case QSConstants.TILE_COMPASS:
+                return new CompassTile(this);
+            case QSConstants.TILE_NFC:
+                return new NfcTile(this);
+	    case QSConstants.TILE_SCREEN_TIMEOUT:
+                return new ScreenTimeoutTile(this);
+>>>>>>> ef4a6d0... SystemUI : Add screen timeout tile
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
