10e05acea8e890b8072028e9c711358ee73fb26b
Danesh M <daneshm90@gmail.com>
2015-01-27 21:17:42 -0800

    SystemUI : Navbar ring targets
    Change-Id: I113d9d257ffa0cb5a51823bee37bd86bb7f19527
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    	packages/SystemUI/res/values/colors.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
    	packages/SystemUI/res/values/colors.xml
    	packages/SystemUI/res/values/strings.xml

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -71,7 +71,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 121;
+
+    private static final int DATABASE_VERSION = 122;
 
     private Context mContext;
     private int mUserHandle;
@@ -1930,6 +1931,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     settingsToMove, true);
             upgradeVersion = 121;
         }
+
+        if (upgradeVersion < 122) {
+            String[] settingsToMove = Settings.Secure.NAVIGATION_RING_TARGETS;
+
+            moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
+                    settingsToMove, true);
+            upgradeVersion = 122;
+        }
+
         // *** Remember to update DATABASE_VERSION above!
 
         if (upgradeVersion != currentVersion) {
