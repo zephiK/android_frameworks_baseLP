2aaed9f2f8a78ab5ad3be4a04348779dec964a89
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 21:49:21 +0000

    QS: add Screenshot tile (1/2)
    Change-Id: I5cefcd20391ffe8fb1bc40e92c522500e5315de7
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -44,7 +44,7 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
-import com.android.systemui.qs.tiles.VisualizerTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -284,8 +284,8 @@ public class QSTileHost implements QSTile.Host {
                 return new HotspotTile(this);
             case QSConstants.TILE_NOTIFICATIONS:
                 return new NotificationsTile(this);
-            case QSConstants.TILE_VISUALIZER:
-                return new VisualizerTile(this);
+            case QSConstants.TILE_SCREENSHOT:
+                return new ScreenshotTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
