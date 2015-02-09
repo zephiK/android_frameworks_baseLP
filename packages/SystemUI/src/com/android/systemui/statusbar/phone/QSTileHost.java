91efce46a40e93618794a418947eb7c5b0115f04
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:48:03 +0000

    QS: add Screen off tile
    * Long press for power menu
    
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Ib40968ed38edeb26fa657c69106d446c6ebdb39e
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
+import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
@@ -283,6 +284,8 @@ public class QSTileHost implements QSTile.Host {
                 return new NotificationsTile(this);
     	    case QSConstants.TILE_SCREEN_TIMEOUT:
                 return new ScreenTimeoutTile(this);
+    	    case QSConstants.TILE_SCREEN_OFF:
+                return new ScreenOffTile(this);
             case QSConstants.TILE_SYNC:
                 return new SyncTile(this);
             case QSConstants.TILE_HEADS_UP:
