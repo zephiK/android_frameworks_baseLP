aa4007bbf3952d2a576db16530f8553ac6319b92
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:55:29 +0000

    QS: add Expanded desktop tile
    Change-Id: Icb4ef73eea923acf9cde52f6c66750a26360f238
    
    Conflicts:
    	packages/SystemUI/res/values/chroma_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -39,6 +39,7 @@ public class QSConstants {
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_BRIGHTNESS = "brightness";
     public static final String TILE_BATTERY_SAVER = "battery_saver";
+    public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -68,5 +69,6 @@ public class QSConstants {
     	TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
         TILES_AVAILABLE.add(TILE_BRIGHTNESS);
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
+        TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
     }
 }
