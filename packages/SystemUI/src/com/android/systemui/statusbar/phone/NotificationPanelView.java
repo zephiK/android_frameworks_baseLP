6733d97bc3821cb225bb2ede18bc4c67f818c446
Danny Baumann <dannybaumann@web.de>
2015-03-06 11:23:05 +0100

    Clean up battery handling.
    Use a single observer for all battery meter and level views, and remove
    no-longer used callback registrations.
    
    Change-Id: I8f9db5e484628d7fbc74fe892d64bef5f08cea24
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1525,7 +1525,6 @@ public class NotificationPanelView extends PanelView implements
 
     private void setListening(boolean listening) {
         mHeader.setListening(listening);
-        mKeyguardStatusBar.setListening(listening);
         mQsPanel.setListening(listening);
     }
 
