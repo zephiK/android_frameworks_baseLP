ee3b832cb2cabf2956df7d1eaf8f007491d47a19
zephiK <zephik@gmail.com>
2015-04-03 03:25:01 -0400

    Update battery, brightness, expanded tiles Credit goes to Altaf-Mahdi (Euphoria), modified to not contain qsCollapsePanel https://github.com/Euphoria-OS/android_frameworks_base/commit/8581f2a90ea7daa7750371f69dff8f8a2013f0ba

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BatterySaverTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BatterySaverTile.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
  * Copyright (C) 2013-2015 The CyanogenMod Project
- * Copyright (C) 2015 The Euphoria-OS Project
+ * Copyright 2014-2015 The Euphoria-OS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,9 +79,9 @@ public class BatterySaverTile extends QSTile<QSTile.BooleanState> {
         mEnabled = powerSaveEnabled();
         state.label = mContext.getString(R.string.quick_settings_battery_saver);
         if (mEnabled) {
-            state.iconId = R.drawable.ic_qs_battery_saver_on;
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_battery_saver_on);
         } else {
-            state.iconId = R.drawable.ic_qs_battery_saver_off;
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_battery_saver_off);
         }
     }
 
