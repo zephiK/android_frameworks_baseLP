0ed00c998e6e3594bb408cd6cf5ddfedbebf6b4a
AragaoAnderson <andersonaragao@me.com>
2015-04-03 14:57:00 -0300

    Framework: Always show ZenPanel when showing expanded volume panel
    This will fit with new 5.1 Stock volume panel and always show ZenPanel when expanded
    
    Thanks to Altaf-Mahdi for the hint!
    
    Change-Id: Ia31afa6bb65510db9874f309c7b47476c87f9f91
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -820,6 +820,7 @@ public class VolumePanel extends Handler implements DemoMode {
                 }
             }
         }
+        updateZenPanelVisible();
     }
 
     private void diappearVolumePanel() {
@@ -1073,7 +1074,8 @@ public class VolumePanel extends Handler implements DemoMode {
     }
 
     private void updateZenPanelVisible() {
-        setZenPanelVisible(mZenModeAvailable && isNotificationOrRing(mActiveStreamType));
+        setZenPanelVisible(mZenModeAvailable && 
+            (isNotificationOrRing(mActiveStreamType) || mExtendedPanelExpanded));
     }
 
     public void postVolumeChanged(int streamType, int flags) {
