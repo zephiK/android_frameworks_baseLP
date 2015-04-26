SphinxOS/frameworks_base

Consider RTL layout in DPAD navigation in AbsSeekBar
Changes the behavior of onKeyDown for DPAD_LEFT/RIGHT when in RTL
to move the progress in same direction as the DPAD key suggest.

Change-Id: I776a48711571884a10ef9315de78bf4ebffd6e4b

Former-commit-id: 575bb3815a62b20e566ed87023b7473eeeb62e05

core/java/android/widget/AbsSeekBar.java
@@ -704,19 +704,20 @@ public abstract class AbsSeekBar extends ProgressBar {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (isEnabled()) {
-            int progress = getProgress();
+            int increment = mKeyProgressIncrement;
             switch (keyCode) {
                 case KeyEvent.KEYCODE_DPAD_LEFT:
-                    if (progress <= 0) break;
-                    animateSetProgress(progress - mKeyProgressIncrement);
-                    onKeyChange();
-                    return true;
-
+                    increment = -increment;
+                    // fallthrough
                 case KeyEvent.KEYCODE_DPAD_RIGHT:
-                    if (progress >= getMax()) break;
-                    animateSetProgress(progress + mKeyProgressIncrement);
-                    onKeyChange();
-                    return true;
+                    increment = isLayoutRtl() ? -increment : increment;
+                    int progress = getProgress() + increment;
+                    if (progress > -mKeyProgressIncrement &&
+                        progress < getMax() + mKeyProgressIncrement) {
+                        animateSetProgress(progress);
+                        onKeyChange();
+                        return true;
+                    }
             }
         }
 

zephiK/android_frameworks_baseLP

Conflicts:
	core/java/android/widget/AbsSeekBar.java

core/java/android/widget/AbsSeekBar.java
@@ -16,6 +16,7 @@
 
 package android.widget;
 
+import android.animation.ObjectAnimator;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.content.res.ColorStateList;
@@ -702,19 +703,20 @@ public abstract class AbsSeekBar extends ProgressBar {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (isEnabled()) {
-            int progress = getProgress();
+            int increment = mKeyProgressIncrement;
             switch (keyCode) {
                 case KeyEvent.KEYCODE_DPAD_LEFT:
-                    if (progress <= 0) break;
-                    setProgress(progress - mKeyProgressIncrement, true);
-                    onKeyChange();
-                    return true;
-
+                    increment = -increment;
+                    // fallthrough
                 case KeyEvent.KEYCODE_DPAD_RIGHT:
-                    if (progress >= getMax()) break;
-                    setProgress(progress + mKeyProgressIncrement, true);
-                    onKeyChange();
-                    return true;
+                    increment = isLayoutRtl() ? -increment : increment;
+                    int progress = getProgress() + increment;
+                    if (progress > -mKeyProgressIncrement &&
+                        progress < getMax() + mKeyProgressIncrement) {
+                        setProgress(progress);
+                        onKeyChange();
+                        return true;
+                    }
             }
         }
 