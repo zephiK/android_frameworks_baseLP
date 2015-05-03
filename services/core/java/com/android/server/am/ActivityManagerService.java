2fc1a914fac45022b169b4eb6897dbdeaf9e83dd
zephiK <zephik@gmail.com>
2015-05-03 10:04:53 -0400

    Revert "AppOps: allow MEDIA_RW uid to start activity"
    This reverts commit fe9ee54a7b15a5297b3f57bc513020b99425e5cb.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -15289,9 +15289,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         // we will switch to the calling user if access to the current user fails.
         int targetUserId = unsafeConvertIncomingUser(userId);
 
-        // There is no definition like MEDIA_RW_UID, so use MEDIA_RW_GID instead.
-        if (callingUid != 0 && callingUid != Process.SYSTEM_UID
-                && callingUid != Process.MEDIA_RW_GID) {
+        if (callingUid != 0 && callingUid != Process.SYSTEM_UID) {
             final boolean allow;
             if (checkComponentPermission(INTERACT_ACROSS_USERS_FULL, callingPid,
                     callingUid, -1, true) == PackageManager.PERMISSION_GRANTED) {
