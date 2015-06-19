ff8003ba65f850a12cb66dbbe5a56b21d265fb9f
zephiK <zephik@gmail.com>
2015-06-19 13:37:02 -0400

    Revert "Allow custom density setting"
    This reverts commit d442773159f60a9ad3842cddcafff58a61008d1c.

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -250,7 +250,6 @@ public class Resources {
             CompatibilityInfo compatInfo, IBinder token) {
         mAssets = assets;
         mMetrics.setToDefaults();
-        mMetrics.updateDensity();
         if (compatInfo != null) {
             mCompatibilityInfo = compatInfo;
         }
@@ -1804,7 +1803,6 @@ public class Resources {
             if (mConfiguration.densityDpi != Configuration.DENSITY_DPI_UNDEFINED) {
                 mMetrics.densityDpi = mConfiguration.densityDpi;
                 mMetrics.density = mConfiguration.densityDpi * DisplayMetrics.DENSITY_DEFAULT_SCALE;
-                mMetrics.updateDensity();
             }
             mMetrics.scaledDensity = mMetrics.density * mConfiguration.fontScale;
 
@@ -2265,7 +2263,7 @@ public class Resources {
             }
             sPreloaded = true;
             mPreloading = true;
-            sPreloadedDensity = DisplayMetrics.getDeviceDensity();
+            sPreloadedDensity = DisplayMetrics.DENSITY_DEVICE;
             mConfiguration.densityDpi = sPreloadedDensity;
             updateConfiguration(null, null);
         }
