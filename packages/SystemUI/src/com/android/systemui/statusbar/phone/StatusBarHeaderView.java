ec448731a1f33b68c86d2714d60f4681985bafe4
zephiK <zephik@gmail.com>
2015-06-20 14:42:40 -0400

    Fix settings long press fc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -585,10 +585,8 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     }
 
     private void startSettingsLongClickActivity() {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-	intent.setClassName("com.android.settings",
-            "com.android.settings.Settings$QSTilesSettingsActivity");
-        mActivityStarter.startActivity(intent, true /* dismissShade */);
+        mActivityStarter.startActivity(new Intent("android.settings.QS_TILE_SETTINGS"),
+                true /* dismissShade */);
     }
 
     private void startBatteryActivity() {
