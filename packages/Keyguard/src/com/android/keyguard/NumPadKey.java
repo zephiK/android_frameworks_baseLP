57bf19d2e20239f03781ac1459881e768612abe4
zephiK <zephik@gmail.com>
2015-06-25 19:17:23 -0400

    Fix build

--- packages/Keyguard/src/com/android/keyguard/NumPadKey.java
+++ packages/Keyguard/src/com/android/keyguard/NumPadKey.java
@@ -105,14 +105,6 @@ public class NumPadKey extends ViewGroup {
         setContentDescription(mDigitText.getText().toString() + mKlondikeText.getText().toString());
     }
 
-    @Override
-    public void onDetachedFromWindow() {
-        super.onDetachedFromWindow();
-
-        // Reset the "announced headset" flag when detached.
-        ObscureSpeechDelegate.sAnnouncedHeadset = false;
-    }
-
     public void setDigit(int digit) {
         mDigit = digit;
         updateText();
