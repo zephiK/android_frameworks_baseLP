a742578694faca244e7b5993b14f156e4cac9c2d
Roman Birg <roman@cyngn.com>
2015-07-10 14:55:34 -0700

    SettingsProvider: add overlay for SWAP_VOLUME_KEYS_ON_ROTATION
    REF: CYNGNOS-425
    Change-Id: I5ce5971f0ba6e8a73bb5926ebce7832fa357252f
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2437,6 +2437,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
            loadHeadsUpSetting(stmt);
 
 
+            loadBooleanSetting(stmt, Settings.System.SWAP_VOLUME_BUTTONS,
+                    R.bool.def_swap_volume_buttons);
+
         } finally {
             if (stmt != null) stmt.close();
         }
